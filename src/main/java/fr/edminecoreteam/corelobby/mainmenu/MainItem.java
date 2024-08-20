9e26abca112014dd824f7e66d4ff78651c747664
Ygroxie <ygroxie34@gmail.com>
2024-08-21 01:23:44 +0200

    Switch All MainItem.java for use ItemBuilder of API and optimize code

--- src/main/java/fr/edminecoreteam/corelobby/mainmenu/MainItem.java
+++ src/main/java/fr/edminecoreteam/corelobby/mainmenu/MainItem.java
@@ -1,5 +1,6 @@
 package fr.edminecoreteam.corelobby.mainmenu;
 
+import fr.edminecoreteam.api.utils.builder.ItemBuilder;
 import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
@@ -18,8 +19,6 @@ import fr.edminecoreteam.corelobby.utils.SkullNBT;
 
 public class MainItem implements Listener
 {
-    private static ItemStack getSkull(String url) { return SkullNBT.getSkull(url); }
-
     @EventHandler
     public void onInventoryClick(InventoryClickEvent e) {
         Player p = (Player)e.getWhoClicked();
@@ -27,25 +26,7 @@ public class MainItem implements Listener
         if (it == null) {  return; }
         if (it.getType() == Material.SKULL_ITEM)
         {
-            if(it.getItemMeta().getDisplayName().equalsIgnoreCase("§b§lMenu Principal §7• Clique"))
-            {
-                e.setCancelled(true);
-                p.playSound(p.getLocation(), Sound.CLICK, 0.5f, 0.5f);
-                MainGui.gui(p);
-            }
-            if(it.getItemMeta().getDisplayName().equalsIgnoreCase("§b§lMain Menu §7• Click"))
-            {
-                e.setCancelled(true);
-                p.playSound(p.getLocation(), Sound.CLICK, 0.5f, 0.5f);
-                MainGui.gui(p);
-            }
-            if(it.getItemMeta().getDisplayName().equalsIgnoreCase("§b§lMenú principal §7• Clic"))
-            {
-                e.setCancelled(true);
-                p.playSound(p.getLocation(), Sound.CLICK, 0.5f, 0.5f);
-                MainGui.gui(p);
-            }
-            if(it.getItemMeta().getDisplayName().equalsIgnoreCase("§b§lHauptmenü §7• Sklick"))
+            if(it.getItemMeta().getDisplayName().equalsIgnoreCase("§b§lMenu Principal §7• Clique") || it.getItemMeta().getDisplayName().equalsIgnoreCase("§b§lMain Menu §7• Click") || it.getItemMeta().getDisplayName().equalsIgnoreCase("§b§lMenú principal §7• Clic") || it.getItemMeta().getDisplayName().equalsIgnoreCase("§b§lHauptmenü §7• Sklick"))
             {
                 e.setCancelled(true);
                 p.playSound(p.getLocation(), Sound.CLICK, 0.5f, 0.5f);
@@ -63,32 +44,8 @@ public class MainItem implements Listener
         if (it == null) { return; }
 
         if (it.getType() == Material.SKULL_ITEM
-                && it.getItemMeta().getDisplayName().equalsIgnoreCase("§b§lMenu Principal §7• Clique")
+                && (it.getItemMeta().getDisplayName().equalsIgnoreCase("§b§lMenu Principal §7• Clique") || it.getItemMeta().getDisplayName().equalsIgnoreCase("§b§lMain Menu §7• Click") || it.getItemMeta().getDisplayName().equalsIgnoreCase("§b§lMenú principal §7• Clic") || it.getItemMeta().getDisplayName().equalsIgnoreCase("§b§lHauptmenü §7• Sklick"))
                 && (a == Action.LEFT_CLICK_AIR || a == Action.LEFT_CLICK_BLOCK || a == Action.RIGHT_CLICK_AIR || a == Action.RIGHT_CLICK_BLOCK))
-        {
-            e.setCancelled(true);
-            p.playSound(p.getLocation(), Sound.CLICK, 0.5f, 0.5f);
-            MainGui.gui(p);
-        }
-        if (it.getType() == Material.SKULL_ITEM
-                && it.getItemMeta().getDisplayName().equalsIgnoreCase("§b§lMain Menu §7• Click")
-                && (a == Action.RIGHT_CLICK_AIR || a == Action.RIGHT_CLICK_BLOCK || a == Action.LEFT_CLICK_AIR || a == Action.LEFT_CLICK_BLOCK))
-        {
-            e.setCancelled(true);
-            p.playSound(p.getLocation(), Sound.CLICK, 0.5f, 0.5f);
-            MainGui.gui(p);
-        }
-        if (it.getType() == Material.SKULL_ITEM
-                && it.getItemMeta().getDisplayName().equalsIgnoreCase("§b§lMenú principal §7• Clic")
-                && (a == Action.RIGHT_CLICK_AIR || a == Action.RIGHT_CLICK_BLOCK || a == Action.LEFT_CLICK_AIR || a == Action.LEFT_CLICK_BLOCK))
-        {
-            e.setCancelled(true);
-            p.playSound(p.getLocation(), Sound.CLICK, 0.5f, 0.5f);
-            MainGui.gui(p);
-        }
-        if (it.getType() == Material.SKULL_ITEM
-                && it.getItemMeta().getDisplayName().equalsIgnoreCase("§b§lHauptmenü §7• Sklick")
-                && (a == Action.RIGHT_CLICK_AIR || a == Action.RIGHT_CLICK_BLOCK || a == Action.LEFT_CLICK_AIR || a == Action.LEFT_CLICK_BLOCK))
         {
             e.setCancelled(true);
             p.playSound(p.getLocation(), Sound.CLICK, 0.5f, 0.5f);
@@ -98,42 +55,7 @@ public class MainItem implements Listener
 
     @EventHandler
     private void onJoin(PlayerJoinEvent e) {
-
-        Player p = e.getPlayer();
-        SettingInfo settingInfo = new SettingInfo(p);
-
-        if (settingInfo.getLang() == 0)
-        {
-            ItemStack profil = getSkull("http://textures.minecraft.net/texture/879e54cbe87867d14b2fbdf3f1870894352048dfecd962846dea893b2154c85");
-            SkullMeta profilM = (SkullMeta)profil.getItemMeta();
-            profilM.setDisplayName("§b§lMenu Principal §7• Clique");
-            profil.setItemMeta((ItemMeta)profilM);
-            p.getInventory().setItem(0, profil);
-        }
-        if (settingInfo.getLang() == 1)
-        {
-            ItemStack profil = getSkull("http://textures.minecraft.net/texture/879e54cbe87867d14b2fbdf3f1870894352048dfecd962846dea893b2154c85");
-            SkullMeta profilM = (SkullMeta)profil.getItemMeta();
-            profilM.setDisplayName("§b§lMain Menu §7• Click");
-            profil.setItemMeta((ItemMeta)profilM);
-            p.getInventory().setItem(0, profil);
-        }
-        if (settingInfo.getLang() == 2)
-        {
-            ItemStack profil = getSkull("http://textures.minecraft.net/texture/879e54cbe87867d14b2fbdf3f1870894352048dfecd962846dea893b2154c85");
-            SkullMeta profilM = (SkullMeta)profil.getItemMeta();
-            profilM.setDisplayName("§b§lMenú principal §7• Clic");
-            profil.setItemMeta((ItemMeta)profilM);
-            p.getInventory().setItem(0, profil);
-        }
-        if (settingInfo.getLang() == 3)
-        {
-            ItemStack profil = getSkull("http://textures.minecraft.net/texture/879e54cbe87867d14b2fbdf3f1870894352048dfecd962846dea893b2154c85");
-            SkullMeta profilM = (SkullMeta)profil.getItemMeta();
-            profilM.setDisplayName("§b§lHauptmenü §7• Sklick");
-            profil.setItemMeta((ItemMeta)profilM);
-            p.getInventory().setItem(0, profil);
-        }
+        giveItem(e.getPlayer());
     }
 
     public static void giveItem(Player p)
@@ -142,34 +64,34 @@ public class MainItem implements Listener
 
         if (settingInfo.getLang() == 0)
         {
-            ItemStack profil = getSkull("http://textures.minecraft.net/texture/879e54cbe87867d14b2fbdf3f1870894352048dfecd962846dea893b2154c85");
-            SkullMeta profilM = (SkullMeta)profil.getItemMeta();
-            profilM.setDisplayName("§b§lMenu Principal §7• Clique");
-            profil.setItemMeta((ItemMeta)profilM);
+            ItemStack profil = new ItemBuilder(Material.SKULL_ITEM, 1, (byte) 3)
+                    .setName("§b§lMenu Principal §7• Clique")
+                    .setSkullUrl("https://textures.minecraft.net/texture/879e54cbe87867d14b2fbdf3f1870894352048dfecd962846dea893b2154c85")
+                    .toItemStack();
             p.getInventory().setItem(0, profil);
         }
         if (settingInfo.getLang() == 1)
         {
-            ItemStack profil = getSkull("http://textures.minecraft.net/texture/879e54cbe87867d14b2fbdf3f1870894352048dfecd962846dea893b2154c85");
-            SkullMeta profilM = (SkullMeta)profil.getItemMeta();
-            profilM.setDisplayName("§b§lMain Menu §7• Click");
-            profil.setItemMeta((ItemMeta)profilM);
+            ItemStack profil = new ItemBuilder(Material.SKULL_ITEM, 1, (byte) 3)
+                    .setName("§b§lMain Menu §7• Click")
+                    .setSkullUrl("https://textures.minecraft.net/texture/879e54cbe87867d14b2fbdf3f1870894352048dfecd962846dea893b2154c85")
+                    .toItemStack();
             p.getInventory().setItem(0, profil);
         }
         if (settingInfo.getLang() == 2)
         {
-            ItemStack profil = getSkull("http://textures.minecraft.net/texture/879e54cbe87867d14b2fbdf3f1870894352048dfecd962846dea893b2154c85");
-            SkullMeta profilM = (SkullMeta)profil.getItemMeta();
-            profilM.setDisplayName("§b§lMenú principal §7• Clic");
-            profil.setItemMeta((ItemMeta)profilM);
+            ItemStack profil = new ItemBuilder(Material.SKULL_ITEM, 1, (byte) 3)
+                    .setName("§b§lMenú principal §7• Clic")
+                    .setSkullUrl("https://textures.minecraft.net/texture/879e54cbe87867d14b2fbdf3f1870894352048dfecd962846dea893b2154c85")
+                    .toItemStack();
             p.getInventory().setItem(0, profil);
         }
         if (settingInfo.getLang() == 3)
         {
-            ItemStack profil = getSkull("http://textures.minecraft.net/texture/879e54cbe87867d14b2fbdf3f1870894352048dfecd962846dea893b2154c85");
-            SkullMeta profilM = (SkullMeta)profil.getItemMeta();
-            profilM.setDisplayName("§b§lHauptmenü §7• Sklick");
-            profil.setItemMeta((ItemMeta)profilM);
+            ItemStack profil = new ItemBuilder(Material.SKULL_ITEM, 1, (byte) 3)
+                    .setName("§b§lHauptmenü §7• Sklick")
+                    .setSkullUrl("https://textures.minecraft.net/texture/879e54cbe87867d14b2fbdf3f1870894352048dfecd962846dea893b2154c85")
+                    .toItemStack();
             p.getInventory().setItem(0, profil);
         }
     }
