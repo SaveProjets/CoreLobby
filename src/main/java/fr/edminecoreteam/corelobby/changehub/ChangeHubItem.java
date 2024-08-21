10e1124f2a9a60ca84420025870cb43176b46c75
Enzo Dijoux <ygroxie34@gmail.com>
2024-08-21 17:31:14 +0200

    Switch All ChangeHubItem.java for use ItemBuilder of API and big optimize on code

--- src/main/java/fr/edminecoreteam/corelobby/changehub/ChangeHubItem.java
+++ src/main/java/fr/edminecoreteam/corelobby/changehub/ChangeHubItem.java
@@ -1,5 +1,6 @@
 package fr.edminecoreteam.corelobby.changehub;
 
+import fr.edminecoreteam.api.utils.builder.ItemBuilder;
 import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
@@ -18,9 +19,6 @@ import fr.edminecoreteam.corelobby.utils.SkullNBT;
 
 public class ChangeHubItem implements Listener
 {
-
-    private static ItemStack getSkull(String url) { return SkullNBT.getSkull(url); }
-
     @EventHandler
     public void onInventoryClick(InventoryClickEvent e) {
         Player p = (Player)e.getWhoClicked();
@@ -29,25 +27,7 @@ public class ChangeHubItem implements Listener
         if (it == null) {  return; }
         if (it.getType() == Material.SKULL_ITEM)
         {
-            if(it.getItemMeta().getDisplayName().equalsIgnoreCase("§d§lChanger De Hub §7• Clique"))
-            {
-                e.setCancelled(true);
-                p.playSound(p.getLocation(), Sound.HORSE_ARMOR, 1.0f, 1.0f);
-                ChangeHubGui.gui(p, 1, changeHubInfo.getServerPageNumber());
-            }
-            if(it.getItemMeta().getDisplayName().equalsIgnoreCase("§d§lChange Hub §7• Click"))
-            {
-                e.setCancelled(true);
-                p.playSound(p.getLocation(), Sound.HORSE_ARMOR, 1.0f, 1.0f);
-                ChangeHubGui.gui(p, 1, changeHubInfo.getServerPageNumber());
-            }
-            if(it.getItemMeta().getDisplayName().equalsIgnoreCase("§d§lCentro de cambios §7• Clic"))
-            {
-                e.setCancelled(true);
-                p.playSound(p.getLocation(), Sound.HORSE_ARMOR, 1.0f, 1.0f);
-                ChangeHubGui.gui(p, 1, changeHubInfo.getServerPageNumber());
-            }
-            if(it.getItemMeta().getDisplayName().equalsIgnoreCase("§d§lHub wechseln §7• Sklick"))
+            if(it.getItemMeta().getDisplayName().equalsIgnoreCase("§d§lChanger De Hub §7• Clique") || it.getItemMeta().getDisplayName().equalsIgnoreCase("§d§lChange Hub §7• Click") || it.getItemMeta().getDisplayName().equalsIgnoreCase("§d§lCentro de cambios §7• Clic") || it.getItemMeta().getDisplayName().equalsIgnoreCase("§d§lHub wechseln §7• Sklick"))
             {
                 e.setCancelled(true);
                 p.playSound(p.getLocation(), Sound.HORSE_ARMOR, 1.0f, 1.0f);
@@ -65,32 +45,8 @@ public class ChangeHubItem implements Listener
         Action a = e.getAction();
         ChangeHubInfo changeHubInfo = new ChangeHubInfo("Lobby");
         if (it.getType() == Material.SKULL_ITEM
-                && it.getItemMeta().getDisplayName().equalsIgnoreCase("§d§lChanger De Hub §7• Clique")
+                && (it.getItemMeta().getDisplayName().equalsIgnoreCase("§d§lChanger De Hub §7• Clique") || it.getItemMeta().getDisplayName().equalsIgnoreCase("§d§lChange Hub §7• Click") || it.getItemMeta().getDisplayName().equalsIgnoreCase("§d§lCentro de cambios §7• Clic") || it.getItemMeta().getDisplayName().equalsIgnoreCase("§d§lHub wechseln §7• Sklick"))
                 && (a == Action.LEFT_CLICK_AIR || a == Action.LEFT_CLICK_BLOCK || a == Action.RIGHT_CLICK_AIR || a == Action.RIGHT_CLICK_BLOCK))
-        {
-            e.setCancelled(true);
-            p.playSound(p.getLocation(), Sound.HORSE_ARMOR, 1.0f, 1.0f);
-            ChangeHubGui.gui(p, 1, changeHubInfo.getServerPageNumber());
-        }
-        if (it.getType() == Material.SKULL_ITEM
-                && it.getItemMeta().getDisplayName().equalsIgnoreCase("§d§lChange Hub §7• Click")
-                && (a == Action.RIGHT_CLICK_AIR || a == Action.RIGHT_CLICK_BLOCK || a == Action.LEFT_CLICK_AIR || a == Action.LEFT_CLICK_BLOCK))
-        {
-            e.setCancelled(true);
-            p.playSound(p.getLocation(), Sound.HORSE_ARMOR, 1.0f, 1.0f);
-            ChangeHubGui.gui(p, 1, changeHubInfo.getServerPageNumber());
-        }
-        if (it.getType() == Material.SKULL_ITEM
-                && it.getItemMeta().getDisplayName().equalsIgnoreCase("§d§lCentro de cambios §7• Clic")
-                && (a == Action.RIGHT_CLICK_AIR || a == Action.RIGHT_CLICK_BLOCK || a == Action.LEFT_CLICK_AIR || a == Action.LEFT_CLICK_BLOCK))
-        {
-            e.setCancelled(true);
-            p.playSound(p.getLocation(), Sound.HORSE_ARMOR, 1.0f, 1.0f);
-            ChangeHubGui.gui(p, 1, changeHubInfo.getServerPageNumber());
-        }
-        if (it.getType() == Material.SKULL_ITEM
-                && it.getItemMeta().getDisplayName().equalsIgnoreCase("§d§lHub wechseln §7• Sklick")
-                && (a == Action.RIGHT_CLICK_AIR || a == Action.RIGHT_CLICK_BLOCK || a == Action.LEFT_CLICK_AIR || a == Action.LEFT_CLICK_BLOCK))
         {
             e.setCancelled(true);
             p.playSound(p.getLocation(), Sound.HORSE_ARMOR, 1.0f, 1.0f);
@@ -104,38 +60,21 @@ public class ChangeHubItem implements Listener
         Player p = e.getPlayer();
         SettingInfo settingInfo = new SettingInfo(p);
 
-        if (settingInfo.getLang() == 0)
-        {
-            ItemStack profil = getSkull("http://textures.minecraft.net/texture/7b7cda83e845f17f96eb686a9edfed3026ac65d74e54bdf283dd09d9c254dee7");
-            SkullMeta profilM = (SkullMeta)profil.getItemMeta();
-            profilM.setDisplayName("§d§lChanger De Hub §7• Clique");
-            profil.setItemMeta((ItemMeta)profilM);
-            p.getInventory().setItem(7, profil);
+        ItemBuilder ib = new ItemBuilder(Material.SKULL_ITEM, 1, (byte) 3)
+                .setSkullUrl("https://textures.minecraft.net/texture/7b7cda83e845f17f96eb686a9edfed3026ac65d74e54bdf283dd09d9c254dee7");
+
+        if(settingInfo.getLang() == 0){
+            ib.setName("§d§lChanger De Hub §7• Clique");
+        } else if (settingInfo.getLang() == 1){
+            ib.setName("§d§lChange Hub §7• Click");
+        }else if (settingInfo.getLang() == 2){
+            ib.setName("§d§lCentro de cambios §7• Clic");
+        }else if (settingInfo.getLang() == 3){
+            ib.setName("§d§lHub wechseln §7• Sklick");
         }
-        if (settingInfo.getLang() == 1)
-        {
-            ItemStack profil = getSkull("http://textures.minecraft.net/texture/7b7cda83e845f17f96eb686a9edfed3026ac65d74e54bdf283dd09d9c254dee7");
-            SkullMeta profilM = (SkullMeta)profil.getItemMeta();
-            profilM.setDisplayName("§d§lChange Hub §7• Click");
-            profil.setItemMeta((ItemMeta)profilM);
-            p.getInventory().setItem(7, profil);
-        }
-        if (settingInfo.getLang() == 2)
-        {
-            ItemStack profil = getSkull("http://textures.minecraft.net/texture/7b7cda83e845f17f96eb686a9edfed3026ac65d74e54bdf283dd09d9c254dee7");
-            SkullMeta profilM = (SkullMeta)profil.getItemMeta();
-            profilM.setDisplayName("§d§lCentro de cambios §7• Clic");
-            profil.setItemMeta((ItemMeta)profilM);
-            p.getInventory().setItem(7, profil);
-        }
-        if (settingInfo.getLang() == 3)
-        {
-            ItemStack profil = getSkull("http://textures.minecraft.net/texture/7b7cda83e845f17f96eb686a9edfed3026ac65d74e54bdf283dd09d9c254dee7");
-            SkullMeta profilM = (SkullMeta)profil.getItemMeta();
-            profilM.setDisplayName("§d§lHub wechseln §7• Sklick");
-            profil.setItemMeta((ItemMeta)profilM);
-            p.getInventory().setItem(7, profil);
-        }
+
+        ItemStack profil = ib.toItemStack();
+        p.getInventory().setItem(7, profil);
     }
 
 }
